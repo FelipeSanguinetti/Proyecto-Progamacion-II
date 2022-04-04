805c70a100f0b7d2af62bbdf35dad207a9be16d2
simondeabelleyra <sdeabelleyra@udesa.edu.ar>
2022-04-04 17:58:55 -0300

    inicio db

--- db/products.js
+++ db/products.js
@@ -1,5 +1,59 @@
-const data = [
-    {}
+const data = {
+    usuario: {},
+    comentarios: [{}],
+    products: [
+    {
+        id: 1,
+        nombre: 'Pizza Cabutia Azul',
+        descripcion: 'Pizza napolitana de fior di latte, cabutia, queso azul y albahaca.',
+        fecha: ''
+    }, {
+        id: 2,
+        nombre: ,
+        descripcion: ,
+        fecha: 
+    }, {
+        id: 3,
+        nombre: ,
+        descripcion: ,
+        fecha: 
+    }, {
+        id: 4,
+        nombre: ,
+        descripcion: ,
+        fecha: 
+    }, {
+        id: 5,
+        nombre: ,
+        descripcion: ,
+        fecha: 
+    }, {
+        id: 6,
+        nombre: ,
+        descripcion: ,
+        fecha: 
+    }, {
+        id: 7,
+        nombre: ,
+        descripcion: ,
+        fecha: 
+    }, {
+        id: 8,
+        nombre: ,
+        descripcion: ,
+        fecha: 
+    }, {
+        id: 9,
+        nombre: ,
+        descripcion: ,
+        fecha: 
+    }, {
+        id: 10,
+        nombre: ,
+        descripcion: ,
+        fecha: 
+    },
 ]
+}
 
 module.exports = data;
