0ce19e756fd08f0d1449c68fb9620ccd2da07113
FelipeSanguinetti <fsanguinetti@udesa.edu.ar>
2022-04-11 15:38:32 -0300

    cambios en products routes y producController

--- controller/productController.js
+++ controller/productController.js
@@ -5,26 +5,18 @@ const controller = {
     product: function(req, res){
         return res.render('product', {
             title: 'Product',
+            products: products.products,
+             id: req.params.id,
             products: products.products
-            
        
 
         })
     },
 
-   product: function(req,res){
-
-       
-       
-        return res.render('product', {
-            title: 'Product',
-            id: req.params.id,
-            products: products.products
-            
-        })
+  
             
 
-  },
+  
 
 
 };
