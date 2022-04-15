9918e1ad8d2cb2766842e2fc0e368927c045208f
simondeabelleyra <sdeabelleyra@udesa.edu.ar>
2022-04-15 17:26:12 -0300

    correccion render en controllers

--- controller/productController.js
+++ controller/productController.js
@@ -4,7 +4,6 @@ const controller = {
 
     product: function(req, res){
         return res.render('product', {
-            title: 'Product',
             product: products.products[req.params.id - 1],
             comments: products.comentarios,
         })
--- controller/userController.js
+++ controller/userController.js
@@ -3,7 +3,6 @@ var products = require('../db/products');
 const controller = {
     profile: function(req, res){
         return res.render('profile', {
-            title: 'Profile',
             usuario: products.usuarios,
             products: products.products,
             comments: products.comentarios,
